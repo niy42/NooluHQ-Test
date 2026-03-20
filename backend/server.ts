4ad0a88fdcca870b54d47b520f2a2b7245e5bd77
niy42 <obanlaniyi42@gmail.com>
2026-03-20 09:12:58 +0100

    fixed cors policy

--- backend/server.ts
+++ backend/server.ts
@@ -14,9 +14,21 @@ import "./models";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://diag-app.netlify.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   }),
 );
